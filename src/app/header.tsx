5aa73e49d4415d7cccfa9116f3fb23aff0f039ef
홍은택 <hongeuntaek09@gmail.com>
2025-06-12 21:02:14 +0900

    fix: 폰트 수정

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-10 bg-white/30 backdrop-blur-xl h-[70px] items-center flex px-4 border-b border-black/10'>
+    <header className='sticky top-0 z-10 bg-black/5 color backdrop-blur-xl h-[70px] items-center flex px-4 border-b border-black/10'>
       <Link href='/' className='text-2xl font-bold'>
-        MDX Blog
+        HET Blog
       </Link>
     </header>
   );
